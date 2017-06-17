96da0c21bb5b850be391660b2c0791f183f0050b
Zumpo <carlsson889@gmail.com>
2017-06-17 22:26:17 +0200

    Added style to InkFinder

--- client/src/InkFinder.js
+++ client/src/InkFinder.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
-import elasticlunr from 'elasticlunr'
-import FormControl from 'react-bootstrap/lib/FormControl'
-import FormGroup from 'react-bootstrap/lib/FormGroup'
+import elasticlunr from 'elasticlunr';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FontAwesome from 'react-fontawesome';
+
 
 var searchIndex;
 class InkFinder extends Component {
@@ -50,20 +52,45 @@ class InkFinder extends Component {
 
     getSearchResults() {
         //Show just 15 items
-        return this.state.searchResults.slice(0, 15).map((item, index) => (
-            <a href={this.state.shoppingcart[item].url} target="_blank"><h1>{this.state.shoppingcart[item].title}</h1></a>
-        ));
+        if(this.state.searchResults.length > 0) {
+            return this.state.searchResults.slice(0, 15).map((item, index) => (
+                <div className={"stripe-" + index % 2}>
+                    <a href={this.state.shoppingcart[item].url} target="_blank"><div>{this.state.shoppingcart[item].title}</div></a>
+                </div>
+            ));
+        } else {
+            return (
+                <div className="text-center">
+                    <FontAwesome
+                        name='print'
+                        style={{ color: 'black' }}
+                        size='5x'
+                    />
+                    <FontAwesome
+                        name='paint-brush'
+                        style={{ color: 'black' }}
+                        size='5x'
+                    />
+                </div>
+
+            )
+        }
+
     }
 
     render() {
         return (
             <div className="container">
-                <div className="col-xs-6">
-                    <h1>Hello mate!</h1>
-                    <FormGroup>
-                        <FormControl type="text" placeholder="Search" onChange={this.search} />
+                <div className="col-xs-10 col-xs-offset-1">
+                    <div className="col-xs-8 col-xs-offset-2">
+                        <h1 className="text-center">Ink Finder</h1>
+                        <FormGroup >
+                            <FormControl bsSize="large" type="text" placeholder="Search" onChange={this.search} />
+                        </FormGroup>
+                    </div>
+                    <div className="row col-xs-12" style={{margin: "20px", minHeight: "200px", padding:"0px", borderRadius: "3px"}}>
                         {this.getSearchResults()}
-                    </FormGroup>
+                    </div>
                 </div>
             </div>
         );
